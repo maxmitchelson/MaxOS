3645fc101375f73868d24fd45f70688dbe40bcdf
Maxime Mitchelson <39421172+maxmitchelson@users.noreply.github.com>
2025-07-06 20:41:05 -0400

    Implemented cleaner limine interface and basic framebuffer wrapper

---
+++ src/limine.rs
@@ -0,0 +1,75 @@
+use crate::frambuffer::{Framebuffer, FramebufferInfo};
+use core::slice;
+use limine::BaseRevision;
+use limine::framebuffer::MemoryModel;
+use limine::request::{FramebufferRequest, HhdmRequest, MemoryMapRequest, RsdpRequest};
+use limine::request::{RequestsEndMarker, RequestsStartMarker};
+
+/// Marks one or more static Limine bootloader request items to be placed in the
+/// `.limine_requests` section of the binary.
+///
+/// This macro automatically applies the required attributes:
+/// - `#[used]` to prevent the linker from discarding the symbol
+/// - `#[link_section = ".limine_requests"]` (via `#[unsafe(...)]`) to ensure it is
+///   placed in the section the Limine bootloader expects.
+macro_rules! limine_request {
+    ($($item:item)*) => {
+        $(
+            #[used]
+            #[unsafe(link_section = ".limine_requests")]
+            $item
+        )*
+    };
+}
+
+/// Marker for the beginning of the Limine bootloader request section
+#[used]
+#[unsafe(link_section = ".limine_requests_start")]
+static _LIMINE_REQUESTS_START_MARKER: RequestsStartMarker = RequestsStartMarker::new();
+
+/// Marker for the end of the Limine bootloader request section
+#[used]
+#[unsafe(link_section = ".limine_requests_end")]
+static _LIMINE_REQUESTS_END_MARKER: RequestsEndMarker = RequestsEndMarker::new();
+
+limine_request! {
+    static BASE_REVISION: BaseRevision = BaseRevision::with_revision(3);
+    static RSDP_REQUEST: RsdpRequest = RsdpRequest::new();
+    static HHDM_REQUEST: HhdmRequest = HhdmRequest::new();
+    static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
+    static MMAP_REQUEST: MemoryMapRequest = MemoryMapRequest::new();
+}
+
+pub fn ensure_base_revision_support() {
+    assert!(BASE_REVISION.is_valid());
+    assert!(BASE_REVISION.is_supported());
+}
+
+pub fn get_rsdp_address() -> usize {
+    RSDP_REQUEST.get_response().unwrap().address()
+}
+
+pub fn get_physical_memory_offset() -> usize {
+    HHDM_REQUEST.get_response().unwrap().offset() as usize
+}
+
+pub fn get_framebuffer<'a>() -> Framebuffer<'a> {
+    let limine_framebuffers = FRAMEBUFFER_REQUEST.get_response().unwrap().framebuffers();
+
+    for buffer in limine_framebuffers {
+        let info = FramebufferInfo::from_limine_framebuffer(&buffer);
+        let size = info.pitch * info.height;
+
+        if buffer.memory_model() != MemoryModel::RGB || info.bytes_per_pixel != 4 {
+            continue; // incompatible pixel layout
+        }
+
+        let buffer_slice = unsafe { slice::from_raw_parts_mut(buffer.addr() as *mut u32, size) };
+        return Framebuffer {
+            info,
+            buffer: buffer_slice,
+        };
+    }
+
+    panic!("No valid framebuffer found");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,115 +1,45 @@
 #![no_std]
 #![no_main]
 
+mod frambuffer;
+mod limine;
+
 use core::arch::asm;
 use core::panic::PanicInfo;
-// use core::ptr::NonNull;
-//
-// use acpi::fadt::Fadt;
-// use acpi::{AcpiHandler, AcpiTables};
-use limine::BaseRevision;
-use limine::request::{
-    FramebufferRequest, /* HhdmRequest, */ RequestsEndMarker, RequestsStartMarker, RsdpRequest,
-};
 
-macro_rules! limine_requests {
-    ($($item:item)*) => {
-        $(
-            #[used]
-            #[unsafe(link_section = ".limine_requests")]
-            $item
-        )*
-    };
-}
-
-limine_requests! {
-    static BASE_REVISION: BaseRevision = BaseRevision::new();
-    static RSDP_REQUEST: RsdpRequest = RsdpRequest::new();
-    // static HHDM_REQUEST: HhdmRequest = HhdmRequest::new();
-    static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
-}
-
-// Define the start and end markers for Limine requests
-#[used]
-#[unsafe(link_section = ".limine_requests_start")]
-static _LIMINE_REQUESTS_START_MARKER: RequestsStartMarker = RequestsStartMarker::new();
-
-#[used]
-#[unsafe(link_section = ".limine_requests_end")]
-static _LIMINE_REQUESTS_END_MARKER: RequestsEndMarker = RequestsEndMarker::new();
+use crate::frambuffer::RGB;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    assert!(BASE_REVISION.is_supported());
+    limine::ensure_base_revision_support();
+    let mut framebuffer = limine::get_framebuffer();
 
-    if let Some(framebuffer_response) = FRAMEBUFFER_REQUEST.get_response() {
-        if let Some(framebuffer) = framebuffer_response.framebuffers().next() {
-            for i in 10..200_u64 {
-                for j in 0..30 {
-                    let pixel_offset = i * framebuffer.pitch() + (i + j - 3) * 4;
-                    unsafe {
-                        framebuffer
-                            .addr()
-                            .add(pixel_offset as usize)
-                            .cast::<u32>()
-                            .write(0xFFFFFFFF)
-                    }
-                }
+
+    let h = framebuffer.info.height / 256;
+    let gap = (framebuffer.info.height - (h * 256)) / 2;
+
+    for x in 0..framebuffer.info.width {
+        for y in 0..gap {
+            framebuffer.set_pixel_value(x, y, RGB::from_hex(0x000000));
+            framebuffer.set_pixel_value(
+                x,
+                framebuffer.info.height - y - 1,
+                RGB::from_hex(0xFFFFFF),
+            );
+        }
+    }
+
+    for i in 0..256 {
+        for x in 0..framebuffer.info.width {
+            for y in i * h..(i + 1) * h {
+                let color = i as u8;
+                framebuffer.set_pixel_value(x, y + gap, RGB::new(color, color, color));
             }
         }
     }
 
     halt();
 }
-
-// #[derive(Clone)]
-// struct CustomAcpiHandler {
-//     hhdm_offset: usize,
-// }
-//
-// impl CustomAcpiHandler {
-//     fn new(offset: u64) -> Self {
-//         Self {hhdm_offset: offset as usize}
-//     }
-// }
-//
-// impl AcpiHandler for CustomAcpiHandler {
-//     unsafe fn map_physical_region<T>(
-//         &self,
-//         physical_address: usize,
-//         size: usize,
-//     ) -> acpi::PhysicalMapping<Self, T> {
-//         let virt = NonNull::new((physical_address + self.hhdm_offset) as *mut T).unwrap();
-//         unsafe { 
-//             acpi::PhysicalMapping::new(
-//                 physical_address, 
-//                 virt,
-//                 size,
-//                 size,
-//                 self.clone(),
-//             ) 
-//         }
-//     }
-//
-//     fn unmap_physical_region<T>(_region: &acpi::PhysicalMapping<Self, T>) {
-//         // nothing to do, memory is permanently mapped
-//     }
-// }
-//
-// fn shutdown() {
-//     let hhdm_offset = HHDM_REQUEST.get_response().unwrap().offset();
-//     let acpi_handler = CustomAcpiHandler::new(hhdm_offset)
-//
-//     if let Some(rsdp_response) = RSDP_REQUEST.get_response() {
-//
-//         let t = unsafe {
-//             AcpiTables::from_rsdp(acpi_handler, rsdp_response.address())
-//         }.unwrap();
-//
-//
-//         let fadt = t.find_table::<Fadt>().unwrap().get();
-//     }
-// }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
