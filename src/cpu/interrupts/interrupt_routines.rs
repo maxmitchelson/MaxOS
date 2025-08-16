7852d1b4cf2c8dc00ab102fbe68de736b3332cef
Maxime Mitchelson <39421172+maxmitchelson@users.noreply.github.com>
2025-08-16 01:21:12 -0400

    Make faults and aborts panic but simply log inside traps

--- src/cpu/interrupts/interrupt_routines.rs
+++ src/cpu/interrupts/interrupt_routines.rs
@@ -3,35 +3,35 @@ use crate::cpu::interrupts::{
 };
 
 pub(super) extern "x86-interrupt" fn divide_error_handler(stack_frame: ISF) {
-    crate::println!("DIVIDE ERROR INTERRUPT stack_frame: {:?}", stack_frame);
+    panic!("DIVIDE ERROR INTERRUPT stack_frame: {:?}", stack_frame);
 }
 
 pub(super) extern "x86-interrupt" fn debug_handler(stack_frame: ISF) {
-    crate::println!("DEBUG INTERRUPT stack_frame: {:#?}", stack_frame);
+    crate::println!("DEBUG TRAP stack_frame: {:#?}", stack_frame);
 }
 
 pub(super) extern "x86-interrupt" fn non_maskable_interrupt_handler(stack_frame: ISF) {
-    crate::println!("NON-MASKABLE INTERRUPT stack_frame: {:#?}", stack_frame);
+    panic!("NON-MASKABLE INTERRUPT stack_frame: {:#?}", stack_frame);
 }
 
 pub(super) extern "x86-interrupt" fn breakpoint_handler(stack_frame: ISF) {
-    crate::println!("BREAKPOINT INTERRUPT stack_frame: {:#?}", stack_frame);
+    crate::println!("BREAKPOINT TRAP stack_frame: {:#?}", stack_frame);
 }
 
 pub(super) extern "x86-interrupt" fn overflow_handler(stack_frame: ISF) {
-    crate::println!("OVERFLOW INTERRUPT stack_frame: {:#?}", stack_frame);
+    crate::println!("OVERFLOW TRAP stack_frame: {:#?}", stack_frame);
 }
 
 pub(super) extern "x86-interrupt" fn bound_range_exceeded_handler(stack_frame: ISF) {
-    crate::println!("BOUND RANGE INTERRUPT stack_frame: {:#?}", stack_frame);
+    panic!("BOUND RANGE INTERRUPT stack_frame: {:#?}", stack_frame);
 }
 
 pub(super) extern "x86-interrupt" fn invalid_opcode_handler(stack_frame: ISF) {
-    crate::println!("INVALID OPCODE INTERRUPT stack_frame: {:#?}", stack_frame);
+    panic!("INVALID OPCODE INTERRUPT stack_frame: {:#?}", stack_frame);
 }
 
 pub(super) extern "x86-interrupt" fn device_not_available_handler(stack_frame: ISF) {
-    crate::println!(
+    panic!(
         "DEVICE NOT AVAILABLE INTERRUPT stack_frame: {:#?}",
         stack_frame
     );
@@ -45,57 +45,51 @@ pub(super) extern "x86-interrupt" fn double_fault_handler(stack_frame: ISF, erro
 }
 
 pub(super) extern "x86-interrupt" fn invalid_tss_handler(stack_frame: ISF, error: SSErr) {
-    crate::println!(
+    panic!(
         "INVALID TSS INTERRUPT stack_frame: {:#?}, error: {:?}",
-        stack_frame,
-        error
+        stack_frame, error
     );
 }
 
 pub(super) extern "x86-interrupt" fn segment_not_present_handler(stack_frame: ISF, error: SSErr) {
-    crate::println!(
+    panic!(
         "SEGMENT NOT PRESENT INTERRUPT stack_frame: {:#?}, error: {:?}",
-        stack_frame,
-        error
+        stack_frame, error
     );
 }
 
 pub(super) extern "x86-interrupt" fn stack_segment_fault_handler(stack_frame: ISF, error: SSErr) {
-    crate::println!(
+    panic!(
         "STACK SEGMENT FAULT INTERRUPT stack_frame: {:#?}, error: {:?}",
-        stack_frame,
-        error
+        stack_frame, error
     );
 }
 
 pub(super) extern "x86-interrupt" fn general_protx_fault_handler(stack_frame: ISF, error: SSErr) {
-    crate::println!(
+    panic!(
         "GENERAL PROTECTION FAULT INTERRUPT stack_frame: {:?}, error: {:?}",
-        stack_frame,
-        error
+        stack_frame, error
     );
 }
 
 pub(super) extern "x86-interrupt" fn page_fault_handler(stack_frame: ISF, error: PageFaultError) {
-    crate::println!(
+    panic!(
         "PAGE FAULT INTERRUPT stack_frame: {:?}, error: {:?}",
-        stack_frame,
-        error
+        stack_frame, error
     );
 }
 
 pub(super) extern "x86-interrupt" fn x87_floating_point_exception_handler(stack_frame: ISF) {
-    crate::println!(
+    panic!(
         "x87 FLOATING POINT EXCEPTION INTERRUPT stack_frame: {:#?}",
         stack_frame
     );
 }
 
 pub(super) extern "x86-interrupt" fn alignement_check_handler(stack_frame: ISF, error: usize) {
-    crate::println!(
+    panic!(
         "ALIGNMENT CHECK INTERRUPT stack_frame: {:#?}, error: {}",
-        stack_frame,
-        error
+        stack_frame, error
     );
 }
 
@@ -104,23 +98,22 @@ pub(super) extern "x86-interrupt" fn machine_check_handler(stack_frame: ISF) -> 
 }
 
 pub(super) extern "x86-interrupt" fn simd_floating_point_handler(stack_frame: ISF) {
-    crate::println!(
+    panic!(
         "SIMD FLOATING POINT INTERRUPT stack_frame: {:#?}",
         stack_frame,
     );
 }
 
 pub(super) extern "x86-interrupt" fn virtualization_exception_handler(stack_frame: ISF) {
-    crate::println!(
+    panic!(
         "VIRTUALIZATION EXCEPTION INTERRUPT stack_frame: {:#?}",
         stack_frame,
     );
 }
 
 pub(super) extern "x86-interrupt" fn ctrl_protx_exception_handler(stack_frame: ISF, error: usize) {
-    crate::println!(
+    panic!(
         "CONTROL PROTECTION EXCEPTION INTERRUPT stack_frame: {:#?}, error: {}",
-        stack_frame,
-        error
+        stack_frame, error
     );
 }
