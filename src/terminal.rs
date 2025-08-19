f571ea4dde31cc6877005e564531067bd3bbd389
Maxime Mitchelson <39421172+maxmitchelson@users.noreply.github.com>
2025-08-19 00:11:05 -0400

    Removed deprecated print and println macros

--- src/terminal.rs
+++ src/terminal.rs
@@ -3,26 +3,3 @@ pub mod logger;
 mod themes;
 
 pub use implementation::*;
-
-macro_rules! print {
-    ($($arg:tt)*) => {{
-        let mut writer = $crate::terminal::TerminalWriter::new();
-        use core::fmt::Write;
-        let _ = write!(writer, $($arg)*);
-    }};
-}
-
-macro_rules! println {
-    () => {
-        $crate::print!("\n")
-    };
-    ($fmt:expr) => {
-        $crate::print!(concat!($fmt, "\n"))
-    };
-    ($fmt:expr, $($arg:tt)*) => {
-        $crate::print!(concat!($fmt, "\n"), $($arg)*)
-    };
-}
-
-pub(crate) use print;
-pub(crate) use println;
